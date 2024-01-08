a97f1c59acfcbc2f70f7cf955651f2e886ef2e04
mcutura <mcutura@student.42berlin.de>
2024-01-08 21:06:19 +0100

    implementation for list - needs optimizing comparison count

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -30,6 +30,64 @@ static size_t	jacobsthal(unsigned int n)
 	return (jacobsthal_cache[n]);
 }
 
+////////////////////////////////////////////////////////////////////////////////
+// Subclass: Tree
+
+/* Canonical */
+Tree::Tree() : root_(0) {}
+Tree::~Tree() {}
+Tree::Tree(Tree const &rhs) : root_(rhs.root()), subtrees_(rhs.branches()) {}
+Tree &Tree::operator=(Tree const &rhs)
+{
+	if (this != &rhs) {
+		this->root_ = rhs.root();
+		this->subtrees_ = rhs.branches();
+	}
+	return *this;
+}
+
+/* Additional constructors */
+Tree::Tree(int n) : root_(n) {}
+Tree::Tree(int a, int b)
+{
+	if (a < b) {
+		this->root_ = b;
+		this->subtrees_.push_back(Tree(a));
+	} else {
+		this->root_ = a;
+		this->subtrees_.push_back(Tree(b));
+	}
+	++g_comparisons;
+}
+
+/* Operator overloads */
+bool Tree::operator<(Tree const &rhs) const
+{
+	++g_comparisons;
+	return (this->root_ < rhs.root());
+}
+
+/* Getters */
+int Tree::root() const { return this->root_; }
+std::list<Tree> const &Tree::branches() const { return this->subtrees_; }
+
+/* Methods */
+void Tree::add_branch(Tree const &branch) { this->subtrees_.push_back(branch); }
+Tree Tree::pop_branch(void)
+{
+	Tree	branch(this->subtrees_.back());
+	this->subtrees_.pop_back();
+	return branch;
+}
+
+/* Static methods */
+Tree Tree::convert_to(int n) { return Tree(n); }
+int Tree::convert_from(Tree const &tree) { return tree.root(); }
+
+////////////////////////////////////////////////////////////////////////////////
+// Substruct: Group
+
+/* Same as Tree, but using std::vector */
 struct Group
 {
 	int					hi;
@@ -72,26 +130,23 @@ struct Group
 		++g_comparisons;
 		return (this->hi < rhs.hi);
 	}
+	static Group convert_to(int n) { return Group(n); }
+	static int convert_from(Group const &group) { return group.hi; }
 };
 
 static void	fjmi_sort_vector(std::vector<Group> &groups);
+static void	fjmi_sort_list(std::list<Tree> &list);
 
-PmergeMe::PmergeMe() : unsorted_()
-{}
+PmergeMe::PmergeMe() : unsorted_() {}
 
-PmergeMe::~PmergeMe()
-{}
+PmergeMe::~PmergeMe() {}
 
-void PmergeMe::add(int n)
-{
-	unsorted_.push_back(n);
-}
+void PmergeMe::add(int n) { unsorted_.push_back(n); }
 
 void PmergeMe::sort(void)
 {
 	struct timespec	start, end;
 
-	this->create_containers();
 	std::cout << "Before:";
 	for (std::vector<int>::const_iterator it = unsorted_.begin();
 	it != unsorted_.end(); ++it) {
@@ -105,21 +160,27 @@ void PmergeMe::sort(void)
 	it != container1_.end(); ++it) {
 		std::cout << ' ' << *it;
 	}
-	std::cout << std::endl
-		<< "Time to process a range of " << unsorted_.size()
+	std::cout << std::endl;
+	std::cout << "Time to process a range of " << unsorted_.size()
 		<< " elements with std::vector : "
 		<< (end.tv_sec - start.tv_sec) * 1000
 		+ (end.tv_nsec - start.tv_nsec) / 1000
 		<< "us" << std::endl;
-	// std::cout << "Comparisons: " << g_comparisons << std::endl;
-}
+	std::cout << "Comparisons: " << g_comparisons << std::endl;
+	g_comparisons = 0;
+	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
+	this->sort_second();
+	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);
+	std::cout << "Time to process a range of " << unsorted_.size()
+		<< " elements with std::list : "
+		<< (end.tv_sec - start.tv_sec) * 1000
+		+ (end.tv_nsec - start.tv_nsec) / 1000
+		<< "us" << std::endl;
+	std::cout << "Comparisons: " << g_comparisons << std::endl;
+	g_comparisons = 0;
 
-void PmergeMe::create_containers()
-{
-	this->container1_.reserve(this->unsorted_.size());
-	for (std::vector<int>::const_iterator it = this->unsorted_.begin();
-	it != this->unsorted_.end(); ++it) {
-		this->container1_.push_back(*it);
+	if (!this->validate()) {
+		std::cerr << "ERROR Sorting elements failed" << std::endl;
 	}
 }
 
@@ -127,20 +188,50 @@ void PmergeMe::sort_first()
 {
 	std::vector<Group>	groups;
 
-	if (this->container1_.size() < 2) { return; }
-	groups.reserve(this->container1_.size());
-	for (std::vector<int>::const_iterator it = this->container1_.begin();
-	it != this->container1_.end(); ++it) {
-		groups.push_back(Group(*it));
-	}
+	groups.reserve(this->unsorted_.size());
+	std::transform(this->unsorted_.begin(), this->unsorted_.end(),
+				std::back_inserter(groups), Group::convert_to);
 	fjmi_sort_vector(groups);
-	for (size_t i = 0; i < groups.size(); ++i) {
-		this->container1_[i] = groups[i].hi;
-	}
+	this->container1_.reserve(this->unsorted_.size());
+	std::transform(groups.begin(), groups.end(),
+				std::back_inserter(this->container1_), Group::convert_from);
 }
 
 void PmergeMe::sort_second()
-{}
+{
+	std::list<Tree>	tree;
+
+	std::transform(this->unsorted_.begin(), this->unsorted_.end(),
+			std::back_inserter(tree), Tree::convert_to);
+	fjmi_sort_list(tree);
+	std::transform(tree.begin(), tree.end(),
+			std::back_inserter(this->container2_), Tree::convert_from);
+}
+
+bool PmergeMe::validate() const
+{
+	for (std::vector<int>::const_iterator it = this->container1_.begin() + 1;
+	it != this->container1_.end(); ++it) {
+		if (*it < *(it - 1)) {
+			std::cerr << "First container NOT sorted!\nElement: "
+			<< *it << " found after " << *(it - 1) << std::endl;
+			return false;
+		}
+	}
+	for (std::list<int>::const_iterator it = this->container2_.begin();
+	it != this->container2_.end(); ++it) {
+		std::list<int>::const_iterator	next(it);
+		std::advance(next, 1);
+		if (next == this->container2_.end()) { break; }
+		if (*next < *it) {
+			std::cerr << "Second container NOT sorted!\nElement: "
+			<< *next << " found after " << *it << std::endl;
+			return false;
+		}
+	}
+	return (this->unsorted_.size() == this->container1_.size()
+	&& this->container1_.size() == this->container2_.size());
+}
 
 static void	fjmi_sort_vector(std::vector<Group> &groups)
 {
@@ -149,6 +240,7 @@ static void	fjmi_sort_vector(std::vector<Group> &groups)
 	Group				straggler;
 	std::vector<Group>	pend;
 
+	if (!half) { return; }
 	if (has_straggler) { straggler = groups.back(); }
 	if (half < 2U) {
 		if (groups.size() > 1 && groups[1] < groups[0]) {
@@ -192,6 +284,92 @@ static void	fjmi_sort_vector(std::vector<Group> &groups)
 	}
 	if (has_straggler) {
 		groups.insert(std::upper_bound(groups.begin(), groups.end(), straggler),
+			straggler);
+	}
+}
+
+static void	fjmi_sort_list(std::list<Tree> &list)
+{
+	size_t const	half(list.size() >> 1);
+	bool			has_straggler(list.size() & 1);
+	Tree			straggler;
+
+	if (!half) { return; }
+	if (has_straggler) {
+		straggler = list.back();
+		list.pop_back();
+	}
+	if (half < 2U) {
+		std::list<Tree>::iterator	first(list.begin());
+		std::list<Tree>::iterator	second(list.begin());
+		std::advance(second, 1);
+		if (*second < *first) {
+			list.splice(first, list, second);
+		}
+		if (has_straggler) {
+			list.insert(std::lower_bound(list.begin(), list.end(), straggler),
+				straggler);
+		}
+		return;
+	}
+	std::list<Tree>::iterator			front(list.begin());
+	std::list<Tree>::reverse_iterator	back(list.rbegin());
+	size_t	i(0);
+	while (i++ < half) {
+		if (*front < *back) {
+			back->add_branch(*front);
+			std::iter_swap(front, back);
+		} else {
+			front->add_branch(*back);
+		}
+		std::advance(front, 1);
+		std::advance(back, 1);
+	}
+	list.resize(half);
+	fjmi_sort_list(list);
+
+	std::list<Tree>				pend;
+	std::list<Tree>::iterator	next(list.begin());
+	size_t						done(1);
+	int							series(2);
+	list.insert(next, next->pop_branch());
+	while (done < list.size()) {
+		size_t	offset(jacobsthal(++series));
+		if (offset >= list.size()) {
+			offset = list.size() - 1;
+		}
+		std::advance(next, offset - std::distance(list.begin(), next));
+		while (std::distance(list.begin(), next) - done) {
+			pend.push_back(next->pop_branch());
+			std::advance(next, -1);
+		}
+		done = jacobsthal(series);
+	}
+
+	int		count(0);
+	int		prev(0);
+	size_t	offset(3);
+	std::list<Tree>::iterator	lookahead(list.begin());
+	std::advance(lookahead, offset);
+	for (std::list<Tree>::const_iterator it = pend.begin();
+	it != pend.end(); ++it) {
+		list.insert(std::lower_bound(list.begin(), lookahead, *it), *it);
+		++count;
+		if (prev + count < 2 || (prev + count) & (prev + count - 1)) {
+			continue;
+		}
+		prev += count;
+		count = 0;
+		offset += (prev << 1);
+		if (offset < list.size()) {
+			lookahead = list.begin();
+			std::advance(lookahead, offset);
+		} else {
+			lookahead = list.end();
+		}
+	}
+	if (has_straggler) {
+		list.insert(std::lower_bound(list.begin(), list.end(), straggler),
 			straggler);
 	}
 }
--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -10,6 +10,27 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/* Chef Martin's recipe for Ford-Johnson algorithm (aka merge-insertion sort)
+ *
+ * A hybrid of merge sort and binary insertion sort, this algorithm aims to
+ * minimize the number of comparisons performed while sorting elements
+ *
+ * Simplified instructions:
+	1. Pair up (leave the odd egg out)
+	2. sort in pairs
+	3. sort pairs recursively by their higher value in ascending sequence
+	4. insert lower values into sequence using Jacobsthal numbers to select
+		those which would require minimal possible range of comparisons
+	5. drop in that odd egg if any
+	6. place in oven for maximum (nlog(n)-n) comparisons before serving
+	7. enjoy your sorted sequence
+ *
+ * This implementation compares performance of two variants:
+	1. using ONLY std::vector as container to sort
+	2. using ONLY std::list as container to sort
+ * Timed results serve to indicate performance of operations used on containers
+ */
+
 #ifndef PMERGEME_HPP
 # define PMERGEME_HPP
 
@@ -18,6 +39,31 @@
 # include <iostream>
 # include <list>
 # include <vector>
+
+class Tree
+{
+	public:
+		Tree();
+		~Tree();
+		Tree(int n);
+		Tree(int a, int b);
+		Tree(Tree const &rhs);
+
+		Tree &operator=(Tree const &rhs);
+		bool operator<(Tree const &rhs) const;
+
+		int root() const;
+		std::list<Tree> const &branches() const;
+		void add_branch(Tree const &branch);
+		Tree pop_branch(void);
+
+		static Tree convert_to(int n);
+		static int convert_from(Tree const &tree);
+
+	private:
+		int				root_;
+		std::list<Tree>	subtrees_;
+};
 
 class PmergeMe
 {
@@ -29,9 +75,9 @@ class PmergeMe
 		void sort(void);
 
 	private:
-		void create_containers();
 		void sort_first();
 		void sort_second();
+		bool validate() const;
 
 		std::vector<int>	unsorted_;
 		std::vector<int>	container1_;
